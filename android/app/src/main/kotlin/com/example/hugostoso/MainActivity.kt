e66d5bd760bf84454daf539b2b64e553371cf47d
Hugo Loiola de Queiroz <hugoloiola.1@gmail.com>
2022-05-22 19:51:10 -0300

    primeiro commit

---
+++ android/app/src/main/kotlin/com/example/hugostoso/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.hugostoso
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/screens/login.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+export 'login.dart';
+
+class Login extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+    )
+  }
+}
